d1734951beb54c82ab115afb4047c46d7347761f
Paul Flagel <paulflagel@gmail.com>
2021-11-15 15:29:24 +0100

    Added multithreading

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import sys
 import numpy as np
 import time
 import threading
+import logging
 
 from basecamp import Basecamp
 from robots import Robot
 from rocks import Rocks
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 BACKGROUND_COLOR = (234, 213, 178)
 ROBOT_COLOR = (250, 120, 60)
@@ -27,6 +32,12 @@ class Simulation:
         self.nb_robots = nb_robots
         self.nb_rocks = nb_rocks
 
+        # self.thread = threading.Thread(target=self.loop)
+
+    def stop_all_threads(self):
+        for robot in self.container:
+            robot.thread.do_run = False
+
     def start(self):
         pygame.init()
         self.screen = pygame.display.set_mode(
@@ -37,24 +48,25 @@ class Simulation:
         clock = pygame.time.Clock()
 
         # conteneur avec tous les robots de la même équipe
-        container = pygame.sprite.Group()
-        container_rocks = pygame.sprite.Group()
+        self.container = pygame.sprite.Group()
+        self.container_rocks = pygame.sprite.Group()
 
         # quartier général
-        base = Basecamp(self.width // 2, self.height // 2)
+        self.base = Basecamp(self.width // 2, self.height // 2)
 
         # initialisation des robots
         for i in range(self.nb_robots):
             x = (
-                base.x
+                self.base.x
                 + 5
-                - 2 * base.size
+                - 2 * self.base.size
                 + (i * 2 * self.robot_size)
                 - (i // 10 * 10 * 2 * self.robot_size)
             )
-            y = (i // 10) * 2 * self.robot_size + base.y + 50
+            y = (i // 10) * 2 * self.robot_size + self.base.y + 50
             vel = np.random.rand(2) * 2 - 1
             robot = Robot(
+                i,
                 x,
                 y,
                 self.width,
@@ -63,40 +75,50 @@ class Simulation:
                 velocity=vel,
                 size=self.robot_size,
             )
-            container.add(robot)
+            self.container.add(robot)
+            robot.thread.start()
 
         # initialisation des gisements de pierres
         for i in range(self.nb_rocks):
-            x = np.random.randint(0, self.width)
-            y = np.random.randint(0, self.height)
+            # x = np.random.randint(0, self.width)
+            # y = np.random.randint(0, self.height)
+            x, y = 100, 100
             vel = np.zeros(2)
             rock = Rocks(
                 x, y, color=BLACK_COLOR, velocity=vel, radius=np.random.randint(5, 20)
             )
-            container_rocks.add(rock)
+            self.container_rocks.add(rock)
 
         # Lancement de la simulation pour T itérations
         for i in range(self.T):
+            if i % 10 == 0:
+                print(i)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    self.stop_all_threads()
                     sys.exit()
 
-            container.update()
-            container_rocks.update()
+            if i == 100:
+                for agent in self.container:
+                    agent.update(rock_pos=np.array([100, 100]))
+            self.container_rocks.update()
             self.screen.fill(BACKGROUND_COLOR)
 
-            base.draw(self.screen)
-            container_rocks.draw(self.screen)
-            container.draw(self.screen)
+            self.base.draw(self.screen)
+            self.container_rocks.draw(self.screen)
+            self.container.draw(self.screen)
 
             pygame.display.flip()
             clock.tick(30)
+
+        # Stop the thread of each robot
+        self.stop_all_threads()
 
         pygame.quit()
 
 
 if __name__ == "__main__":
     simu = Simulation(WIDTH, HEIGHT)
-    simu.nb_robots = 20
-    simu.nb_rocks = 10
+    simu.nb_robots = 1
+    simu.nb_rocks = 1
     simu.start()
--- robots.py
+++ robots.py
@@ -1,6 +1,10 @@
 import pygame
 import numpy as np
+import threading
+import time
+import logging
 
+logger = logging.getLogger(__name__)
 
 BACKGROUND_COLOR = (234, 213, 178)
 ROBOT_COLOR = (250, 120, 60)
@@ -8,9 +12,10 @@ ROBOT_COLOR = (250, 120, 60)
 
 class Robot(pygame.sprite.Sprite):
     def __init__(
-        self, x, y, width, height, color=ROBOT_COLOR, size=10, velocity=[0, 0]
+        self, id, x, y, width, height, color=ROBOT_COLOR, size=10, velocity=[0, 0]
     ):
         super().__init__()
+        self.id = id
         self.image = pygame.Surface([size, size])
         self.image.fill(BACKGROUND_COLOR)
         pygame.draw.rect(self.image, color, pygame.Rect(0, 0, size, size))
@@ -22,7 +27,12 @@ class Robot(pygame.sprite.Sprite):
         self.width = width
         self.height = height
 
-    def update(self):
+        self.thread = threading.Thread(target=self.loop)
+
+    def update(self, rock_pos=None):
+        if rock_pos is not None:
+            u = rock_pos - self.pos
+            self.vel = u * np.linalg.norm(self.vel) / np.linalg.norm(u)
         self.pos += self.vel
         x, y = self.pos
 
@@ -36,3 +46,10 @@ class Robot(pygame.sprite.Sprite):
         # Update de la position du rectangle
         self.rect.x = x
         self.rect.y = y
+
+    def loop(self):
+        t = threading.currentThread()
+        while getattr(t, "do_run", True):
+            time.sleep(0.025)
+            self.update()
+        logger.debug(f"Thread {self.id} stopped")
